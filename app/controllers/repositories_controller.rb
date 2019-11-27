6c3a1914b1227a05d177c3b02b7c0817594193e3
Will Hodgson <will.hodgson@current-rms.com>
2019-11-27 11:06:38 +0000

    Automatically backed up by Learn

--- app/controllers/repositories_controller.rb
+++ app/controllers/repositories_controller.rb
@@ -6,9 +6,9 @@ class RepositoriesController < ApplicationController
 
   def github_search
     resp = Faraday.get 'https://api.github.com/search/repositories' do |req|
+      req.params['q'] = 'tetris'
       req.params['client_id'] = '88d57525a8243b4c95cc'
       req.params['client_secret'] = 'ebd56e27d043a95d8bdf0e8e37fe8c8cb8eb481a'
-      req.params['q'] = 'tetris'
     end
 
     body = JSON.parse(resp.body)
